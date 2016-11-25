4eeb9ce01f91fbb78b569e947dd2dc5b8c93e4d2
richo225 <richo225@gmail.com>
2016-11-25 00:24:13 +0000

    Setup webhook using localtunnel. Bot works :tada:

--- botmaster.js
+++ botmaster.js
@@ -8,16 +8,15 @@ const telegramSettings = {
   credentials: {
     authToken: process.env.TELEGRAM_AUTH_TOKEN,
   },
-  webhookEndpoint: "/webhook1234",
+  webhookEndpoint: "/webhook1234/",
 };
 
-const botsSettings = [{ telegram: telegramSettings }];
-
 // Bot code
-const botmaster = new Botmaster(botsSettings);
+const botsSettings = [{ telegram: telegramSettings }];
+const botmaster = new Botmaster({botsSettings});
 
 botmaster.on("update", (bot, update) => {
-  bot.sendTextMessageTo("Right back at you!", update.sender.id);
+  bot.reply(update, "Right back at you");
 });
 
 botmaster.on("error", (bot, err) => {
